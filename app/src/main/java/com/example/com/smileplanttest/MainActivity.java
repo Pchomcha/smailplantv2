fda2977e852bf4db647f9be71450c080974b9a22
ple <homepooh@windowslive.com>
2016-02-22 14:57:13 +0700

    tester Add value FInish

--- app/src/main/java/com/example/com/smileplanttest/MainActivity.java
+++ app/src/main/java/com/example/com/smileplanttest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.com.smileplanttest;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,5 +18,32 @@ public class MainActivity extends AppCompatActivity {
         //Request Database
         manageTABLE = new ManageTABLE(this);
 
+        //Setup Data   สำหรับข้อมูลที่ไม่สมบูรณ์
+        forSetupData ();
+
     } //main Method
+
+    private void forSetupData() {
+
+        //CHeck emty Data
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+ ManageTABLE.plant_table, null);
+        if (cursor.getCount() == 0 ) {
+
+            //ไม่มีข้อมูล
+            addFirstData();
+
+        } //if
+
+
+    }  //forSTEupdata
+
+    private void addFirstData() {
+
+        //Tester Add Data
+        manageTABLE.addPlant("nameth", "nameeng", "hprodoc", "hage", "hseason", "hplant",
+                "data", "dround", "plant", "water", "compost,", "potect", "harvest");
+
+    } //addFirsData
 } //Main class
--- app/src/main/java/com/example/com/smileplanttest/ManageTABLE.java
+++ app/src/main/java/com/example/com/smileplanttest/ManageTABLE.java
@@ -1,5 +1,6 @@
 package com.example.com.smileplanttest;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,10 +11,26 @@ public class ManageTABLE {
 
     //Explicit
     private MyOpenHelper myOpenHelper;
-    private SQLiteDatabase writeSqLiteDatabase,readSqLiteDatabase;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String plant_table = "plantTABLE";
+    public static final String colum_id = "_id";
+    public static final String colum_Nameth = "Nameth";
+    public static final String colum_Nameeng = "Nameeng";
+    public static final String colum_HProduc = "HProduc";
+    public static final String colum_HAge = "HAge";
+    public static final String colum_HSeason = "HSeason";
+    public static final String colum_HPlant = "HPlant";
+    public static final String colum_Data = "Data";
+    public static final String colum_ground = "ground";
+    public static final String colum_plant = "plant";
+    public static final String colum_water = "water";
+    public static final String colum_compost = "compost";
+    public static final String colum_protect = "protect";
+    public static final String colum_Harvest = "Harvest";
 
 
-    public ManageTABLE(Context context ) {
+    public ManageTABLE(Context context) {
 
         //create&connected
         myOpenHelper = new MyOpenHelper(context);
@@ -21,4 +38,38 @@ public class ManageTABLE {
         readSqLiteDatabase = myOpenHelper.getReadableDatabase();
 
     } //Constuctor
+
+    public long addPlant(String strNameth,
+                         String strNameeng,
+                         String strHProduc,
+                         String strHAge,
+                         String strHSeason,
+                         String strHPlant,
+                         String strData,
+                         String strground,
+                         String strplant,
+                         String strwater,
+                         String strcompost,
+                         String strprotect,
+                         String strHarvest) {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(colum_Nameth, strNameth);
+        contentValues.put(colum_Nameeng, strNameeng);
+        contentValues.put(colum_HProduc, strHProduc);
+        contentValues.put(colum_HAge, strHAge);
+        contentValues.put(colum_HSeason, strHSeason);
+        contentValues.put(colum_HPlant, strHPlant);
+        contentValues.put(colum_Data, strData);
+        contentValues.put(colum_ground, strground);
+        contentValues.put(colum_plant, strplant);
+        contentValues.put(colum_water, strwater);
+        contentValues.put(colum_compost, strcompost);
+        contentValues.put(colum_protect, strprotect);
+        contentValues.put(colum_Harvest, strHarvest);
+
+        return writeSqLiteDatabase.insert(plant_table,null,contentValues);
+    }
+
+
 }  //Main Class
